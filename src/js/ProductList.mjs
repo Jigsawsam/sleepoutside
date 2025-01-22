a808b0e7b0233ddd290dd1b97e9578426399571b
Jigsawsam <bgt117@gmail.com>
2025-01-22 01:11:34 -0800

    bgt individual 2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,11 +2,11 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return `<li class="product-card">
-      <a href="product_pages/index.html?product=">
-        <img src="" alt="Image of ">
-        <h3 class="card__brand"></h3>
-        <h2 class="card__name"></h2>
-        <p class="product-card__price">$</p>
+      <a href="product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h2 class="card__name">${product.Name}</h2>
+        <p class="product-card__price">$${product.FinalPrice}</p>
       </a>
     </li>`
   }
@@ -24,7 +24,6 @@ export default class ProductListing{
     }
 
     renderList(list) {
-        const htmlStrings = list.map(productCardTemplate);
-        this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-        }
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
 }
